a1c383bf98a7690006f3dafaa96fc8076e1cdf1c
ashna99 <53310520+ashna99@users.noreply.github.com>
2021-04-11 23:32:25 +0530

    added captcha to react/view

--- src/App.js
+++ src/App.js
@@ -1,79 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
-import styled from "styled-components";
-import ReCAPTCHA from "react-google-recaptcha";
-import {AppConfig} from "./configs"
+import React from "react";
+import Form from "./components/form"
 
 
-const AppContainer = styled.div`
-border: 1px solid lightgray;
- display: flex;
- max-width:750px;
- width:90vw;
- margin:30px auto;
- padding: 20px 20px;
-`
-const ReCAPTCHA_Container = styled.div`
-   max-width: 400px;
-   width: 96vw;
-   margin:10px 0px;
-   display: flex;
-   #captcha-expired-info{
-     color: red;
-     font-weight:500;
-   }
-`
-const App = () => {
-  const [token, setToken] = useState(null);
-  const [expired, setExpired] = useState(false);
-  const [error, setError] = useState("");
-  const recaptchaRef = useRef();
-  const verificationHandler = (token) => {
-    //recaptcha expired
-    if (token === null) {
-        setExpired(true);
-      setToken("");
-      return;
-    }
-    setExpired(false);
-    const recaptchaValue = recaptchaRef.current.getValue();
-    setToken(recaptchaValue);
-  };
-  const submissionHandler = (event) =>{
-    event.preventDefault();
-    if(!token){
-      alert('please verify captcha')
-      return;
-    }
-       setError("");
-     recaptchaRef.current.reset();
-
-  }
-  useEffect(() => {
-    console.log(token);
-  }, [setToken]);
-  return (
-    <AppContainer>
-      <div className="app-page">
-        <form onSubmit={(event) => submissionHandler(event)}>
-          <input type="text" placeholder="Enter your email" />
-          <ReCAPTCHA_Container id='grecaptcha-wrapper'>
-          {expired && <div id='captcha-expired-info'>
-            Captcha expired. Please try again
-          </div> 
-          }
-          <ReCAPTCHA
-            sitekey={AppConfig.grecaptcha.siteKey}
-            onChange={(token) => verificationHandler(token)}
-            ref={recaptchaRef}
-            size='normal'
-            theme='dark'
-          />
-          </ReCAPTCHA_Container>
-          <button type='submit' >Submit</button>
-        </form>
-      </div>
-    </AppContainer>
-  );
+const App = () =>{
+  return(
+    <div id='app'>
+     <Form />
+    </div>
+  )
 }
 
 export default App;
